0abd31000ee779c766f10855e15a5b7cd3fb08e9
artin231 <leiartrezjah@gmail.com>
2026-08-13 00:21:17 +0330

    starting the login page

--- Front-end/src/pages/LoginPage.jsx
+++ Front-end/src/pages/LoginPage.jsx
@@ -1,3 +1,23 @@
-export default function LoginPage(){
-    return <h1>hello</h1>
+export default function LoginPage() {
+    return (
+        <>
+            <div className="container">
+                <form action="" className="login">
+                    <h1>ورود</h1>
+                    <div className="item">
+                        <p>نام کاربری</p>
+                        <input type="text" />
+                    </div>
+                    <div className="item">
+                        <p>رمز عبور</p>
+                        <input type="password" />
+                    </div>
+                    <div className="item">
+                        <p>تکرار رمز عبور</p>
+                        <input type="password" />
+                    </div>
+                </form>
+            </div>
+        </>
+    )
 }
