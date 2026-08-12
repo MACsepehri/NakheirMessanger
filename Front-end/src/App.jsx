f00a93a603806c936701b996b92932859722c6c4
artin231 <leiartrezjah@gmail.com>
2026-08-13 00:18:22 +0330

    making the main page

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import NotFoundPage from './pages/Notfound'
+import MainPage from './pages/Mainpage'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='*' element={<NotFoundPage />} />
+        <Route path='/' element={<MainPage />} />
         <Route path='/login' element={<LoginPage />} />
       </Routes>
     </Router>
---
+++ Front-end/src/pages/Mainpage.jsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { redirect, useNavigate } from "react-router-dom";
+
+let MainPage = () => {
+    const navigate = useNavigate();
+    let login = false;
+    useEffect(() => {
+        if (!login) {
+            navigate('/login')
+        }
+    }, [login, navigate])
+
+    return <h1>hello</h1>
+}
+
+export default MainPage;
