4a2985a0cc70f9c798fe7b30481530dadbae0654
artin231 <leiartrezjah@gmail.com>
2026-08-13 00:09:36 +0330

    finishing the not found page css stytles

--- Front-end/src/pages/Notfound.jsx
+++ Front-end/src/pages/Notfound.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
+import '../css/NotFound.css'
 
 let NotFoundPage = () => {
     return (
         <div className="container">
             <div className="box_notfound">
+                <img src="src/assets/orange-error-icon-0.png" alt="" />
                 <h1>صفحه مورد نظر شما پیدا نشد</h1>
                 <Link to='/'>رفتن به صفحه اصلی</Link>
             </div>
