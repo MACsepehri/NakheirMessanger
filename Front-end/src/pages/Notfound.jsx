14123e1187c66de33817f57d30b09b7ca2ecc87d
artin231 <leiartrezjah@gmail.com>
2026-08-13 15:47:31 +0330

    using styled component and using a new theme

--- Front-end/src/pages/Notfound.jsx
+++ Front-end/src/pages/Notfound.jsx
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom"
-import '../css/NotFound.css'
+import styled from "styled-components";
+import '../css/Main.css'
 
 let NotFoundPage = () => {
+
+    let BoxNotFound = styled.div
+        `
+        width: 600px;
+    height: 400px;
+    background-color: #1A1953;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    border-radius: 80px;
+    gap: 40px;
+    color: white;
+    `
+    let ImgBox = styled.img
+    `
+        width: 200px;
+    ` 
     return (
         <div className="container">
-            <div className="box_notfound">
-                <img src="src/assets/orange-error-icon-0.png" alt="" />
+            <BoxNotFound>
+                <ImgBox src="src/assets/orange-error-icon-0.png" alt="" />
                 <h1>صفحه مورد نظر شما پیدا نشد</h1>
                 <Link to='/'>رفتن به صفحه اصلی</Link>
-            </div>
+            </BoxNotFound>
         </div>
     )
 };
