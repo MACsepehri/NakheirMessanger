6dfe628ee91aac17bb3ad43bd2b8b53777c6085c
artin231 <leiartrezjah@gmail.com>
2026-08-15 16:10:10 +0330

    fixed a bug

--- Front-end/src/components/Login_And_Signup/FormAuthSignUpItem.jsx
+++ Front-end/src/components/Login_And_Signup/FormAuthSignUpItem.jsx
@@ -1,39 +1,41 @@
 import { useEffect, useState } from 'react';
-import {ItemDiv,InputItem} from '../../styled_components/StyledLoginPage'
+import { ItemDiv, InputItem } from '../../styled_components/StyledLoginPage'
 
-let FormAuthSignUpItem = ({ ItemName, InputType,IDNAME }) => {
+let FormAuthSignUpItem = ({ ItemName, InputType, IDNAME }) => {
     let users = []
     let CheckInput = () => {
-        if(InputType=='text'){
+        if (InputType == 'text') {
             let input = document.querySelector('.TextInput');
-            users.forEach(i => {                
-                if(i.public_name == input.value){
-                    console.log(input.style);
-                    
-                    input.style.borderBottom = '2px solid red'
-                    document.querySelector('.alertTextInput').innerHTML = 'این نام کاربری وجود دارد'
+            let alertElement = document.querySelector('.alertTextInput');
+            let found = false;
+            users.forEach(i => {
+                if (i.public_name == input.value) {
+                    found = true;
                 }
-                else{
-                    input.style.borderBottom = '2px solid #9112BC'
-                    document.querySelector('.alertTextInput').innerHTML = ''
-                }
-            })         
+            })
+            if (found) {
+                input.style.borderBottom = '2px solid red';
+                alertElement.innerHTML = 'این نام کاربری وجود دارد';
+            } else {
+                input.style.borderBottom = '2px solid #9112BC';
+                alertElement.innerHTML = '';
+            }
         }
     }
     useEffect(() => {
         fetch('http://127.0.0.1:5000/main-login-checker')
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            users = data.users;
-            console.log(users)
-        })
-    },[users])
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                users = data.users;
+                console.log(users)
+            })
+    }, [users])
     return (
         <ItemDiv className="item">
             <p>{ItemName}</p>
-            <InputItem className={`${InputType=='text'?'TextInput':'PasswordInput'}`} id={IDNAME} type={`${InputType}`} onChange={() => {CheckInput()}} />
+            <InputItem className={`${InputType == 'text' ? 'TextInput' : 'PasswordInput'}`} id={IDNAME} type={`${InputType}`} onChange={() => { CheckInput() }} />
             <h6 className={`alert${IDNAME}`}></h6>
         </ItemDiv>
     )
