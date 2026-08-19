2fd6dd13ecc81c2cff3efa6ee957b9ec64523d33
artin231 <leiartrezjah@gmail.com>
2026-08-19 11:23:23 +0330

    adding styles

--- Front-end/src/components/MainPage/Main.jsx
+++ Front-end/src/components/MainPage/Main.jsx
@@ -1,8 +1,12 @@
+import { LeftMainPart,CenterMainPart,RightMainPart } from "../../styled_components/StyledMainPage";
+
 let Main = () => {
     return (
         <>
         <div className="holder">
-            
+            <LeftMainPart></LeftMainPart>
+            <CenterMainPart></CenterMainPart>
+            <RightMainPart></RightMainPart>
         </div>
         </>
     )
---
+++ Front-end/src/styled_components/StyledMainPage.jsx
@@ -0,0 +1,41 @@
+import {styled} from "styled-components";
+
+export let LeftMainPart = styled.div`
+width:20%;
+height:100%;
+border-radius:30px;
+background-color: #17102c ;
+border:2px solid #17102c;
+
+&:hover{
+transform:translateY(-1%);
+border:2px solid #3d335f;
+}
+`
+
+export let CenterMainPart = styled.div`
+width:60%;
+height:100%;
+border-radius:30px;
+background-color: #221941 ;
+border:2px solid #221941;
+
+&:hover{
+transform:translateY(-1%);
+border:2px solid #3d335f;
+}
+`
+
+
+export let RightMainPart = styled.div`
+width:20%;
+height:100%;
+border-radius:30px;
+background-color: #17102c ;
+border:2px solid #17102c;
+
+&:hover{
+transform:translateY(-1%);
+border:2px solid #3d335f;
+}
+`
