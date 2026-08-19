4a0dcdfb2ae15e9af918441687a0d765faae5a99
artin231 <leiartrezjah@gmail.com>
2026-08-19 12:49:36 +0330

    changin bg and the boxes

--- Front-end/src/components/MainPage/Main.jsx
+++ Front-end/src/components/MainPage/Main.jsx
@@ -1,13 +1,18 @@
-import { LeftMainPart,CenterMainPart,RightMainPart } from "../../styled_components/StyledMainPage";
+import { LeftMainPart, CenterMainPart, RightMainPart, Img_avatar, Div_Img } from "../../styled_components/StyledMainPage";
 
 let Main = () => {
     return (
         <>
-        <div className="holder">
-            <LeftMainPart></LeftMainPart>
-            <CenterMainPart></CenterMainPart>
-            <RightMainPart></RightMainPart>
-        </div>
+            <div className="holder">
+                <LeftMainPart>
+                    <Div_Img>
+                        <Img_avatar src="/src/assets/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-illustration-vector.jpg" alt="" srcset="" />
+                        <h1>علی ممدی</h1>
+                    </Div_Img>
+                </LeftMainPart>
+                <CenterMainPart></CenterMainPart>
+                <RightMainPart></RightMainPart>
+            </div>
         </>
     )
 }
--- Front-end/src/styled_components/StyledMainPage.jsx
+++ Front-end/src/styled_components/StyledMainPage.jsx
@@ -4,9 +4,12 @@ export let LeftMainPart = styled.div`
 width:20%;
 height:100%;
 border-radius:30px;
-background-color: #17102c ;
-border:2px solid #17102c;
-
+background-color: #0d0d1f5b ;
+border:2px solid #313164;
+display:flex;
+flex-direction:column;
+padding-top:2%;
+transition:all 600ms ease;
 &:hover{
 transform:translateY(-1%);
 border:2px solid #3d335f;
@@ -14,11 +17,12 @@ border:2px solid #3d335f;
 `
 
 export let CenterMainPart = styled.div`
-width:60%;
+width:50%;
 height:100%;
 border-radius:30px;
-background-color: #221941 ;
-border:2px solid #221941;
+background-color: #0d0d1f5b ;
+border:2px solid #313164;
+transition:all 600ms ease;
 
 &:hover{
 transform:translateY(-1%);
@@ -28,14 +32,32 @@ border:2px solid #3d335f;
 
 
 export let RightMainPart = styled.div`
-width:20%;
+width:30%;
 height:100%;
 border-radius:30px;
-background-color: #17102c ;
-border:2px solid #17102c;
+background-color: #0d0d1f5b ;
+backdrop-filter:blur(100px);
+border:2px solid #313164;
+transition:all 600ms ease;
 
 &:hover{
 transform:translateY(-1%);
 border:2px solid #3d335f;
 }
+`
+
+export let Div_Img = styled.div
+`
+width:100%;
+display:flex;
+align-items : center;
+justify-content:flex-start;
+padding-left:10%;
+gap:3%;
+`
+
+export let Img_avatar = styled.img
+`
+width:50px;
+border-radius:100%;
 `
