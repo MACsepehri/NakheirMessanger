78807ba968c9a9f99b92d3c5b0a3ea87b1ed7298
artin231 <leiartrezjah@gmail.com>
2026-08-21 12:28:08 +0330

    new socket front

--- Front-end/src/components/MainPage/Main.jsx
+++ Front-end/src/components/MainPage/Main.jsx
@@ -3,7 +3,20 @@ import { LeftMainPart, CenterMainPart, RightMainPart, Img_avatar, Div_Img, Avata
 import { io } from 'socket.io-client';
 
 let Main = () => {
-    const socket = io(`http://${window.location.host}`);
+    // const socket = io(`http://${window.location.host}`);
+    // socket.on('connect',() => {
+    //     console.log('connected');
+    // })
+    // socket.on('disconnect',() => {
+    //     console.log('disconnected');
+    // })
+    // console.log(socket.id);
+    
+    // socket.on('chat:message',(message) => {
+    //     console.log(message);
+    // })
+    // let data = {'message':'i am not ali'}
+    // socket.emit('chat:message', data)
     let storedName = JSON.parse(sessionStorage.getItem('user'));
     return (
 
